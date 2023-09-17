558f485d2905b243ca8c2798cda3067d846ff2a1
gliterpoint <freefake476@gmail.com>
2023-09-18 00:19:47 +0500

    Added Resolvers

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+// resolvers
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  // typeDefs,
-  // resolvers,
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server);
